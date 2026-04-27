bb8a936068715d16bbd90c0584b91d86bbc7042c
Winsilv <winnicius.s@escolar.ifrn.edu.br>
2026-04-27 13:28:09 +0000

    update final in capitulo 5

--- loja/views/ProdutoView.py
+++ loja/views/ProdutoView.py
@@ -1,9 +1,6 @@
 from django.http import HttpResponse
 
 def list_produto_view(request, id=None):
-    # Trecho opcional do PDF (página 16) incluído conforme solicitado:
     if id is None:
         return HttpResponse('<h1>Nenhum id foi informado</h1>')
-        # Ou use: id = 0
-
     return HttpResponse('<h1>Produto de id %s!</h1>' % id)
