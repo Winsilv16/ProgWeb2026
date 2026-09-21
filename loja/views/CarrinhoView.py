1e4abb07461902d6aa40f3fd997783efbd04b974
Winsilv <winnicius.s@escolar.ifrn.edu.br>
2026-09-21 12:53:19 +0000

    update in Capítulo 20 - Trabalhando com Variáveis de Sessão

--- loja/views/CarrinhoView.py
+++ loja/views/CarrinhoView.py
@@ -1,25 +1,22 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from django.utils import timezone
 from loja.models import Carrinho, CarrinhoItem, Produto
 
 def create_carrinhoitem_view(request, produto_id):
-    produto = get_object_or_404(Produto, id=produto_id)
     carrinho_id = request.session.get('carrinho_id')
-
-    if carrinho_id:
-        carrinho = Carrinho.objects.filter(id=carrinho_id, situacao=0).first()
-    else:
-        carrinho = None
-
-    if not carrinho:
+    
+    if not carrinho_id:
         carrinho = Carrinho.objects.create()
         request.session['carrinho_id'] = carrinho.id
+    else:
+        carrinho = get_object_or_404(Carrinho, id=carrinho_id, situacao=0)
 
+    produto = get_object_or_404(Produto, id=produto_id)
+    
     item, created = CarrinhoItem.objects.get_or_create(
         carrinho=carrinho,
         produto=produto,
-        defaults={'preco': produto.preco, 'quantidade': 1}
+        defaults={'quantidade': 1, 'preco': produto.preco}
     )
 
     if not created:
@@ -28,25 +25,29 @@ def create_carrinhoitem_view(request, produto_id):
 
     return redirect('list_carrinho')
 
+
 def list_carrinho_view(request):
     carrinho_id = request.session.get('carrinho_id')
-    itens = []
     carrinho = None
+    itens = []
 
     if carrinho_id:
         carrinho = Carrinho.objects.filter(id=carrinho_id, situacao=0).first()
         if carrinho:
-            itens = CarrinhoItem.objects.filter(carrinho=carrinho)
+            itens = carrinho.carrinhoitem_set.all()
 
-    return render(request, 'carrinho/carrinho-listar.html', {'carrinho': carrinho, 'itens': itens})
+    return render(request, 'carrinho/carrinho-listar.html', {
+        'carrinho': carrinho,
+        'itens': itens
+    })
+
 
 def remover_item_view(request, item_id):
-    if request.method == 'POST':
-        carrinho_id = request.session.get('carrinho_id')
-        if carrinho_id:
-            item = get_object_or_404(CarrinhoItem, id=item_id, carrinho_id=carrinho_id)
-            item.delete()
+    carrinho_id = request.session.get('carrinho_id')
+    item = get_object_or_404(CarrinhoItem, id=item_id, carrinho_id=carrinho_id)
+    item.delete()
     return redirect('list_carrinho')
+
 
 @login_required
 def confirmar_carrinho_view(request):
@@ -57,7 +58,6 @@ def confirmar_carrinho_view(request):
     carrinho = get_object_or_404(Carrinho, id=carrinho_id, situacao=0)
     carrinho.user = request.user
     carrinho.situacao = 1
-    carrinho.confirmado_em = timezone.make_aware(timezone.datetime.now())
     carrinho.save()
 
     del request.session['carrinho_id']
